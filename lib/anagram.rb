4dade72c23210d0fc3e3ffabcc4f44af5849210a
craig_suess <suesscraig112715@gmail.com>
2021-10-20 20:50:29 -0400

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :anagram
+    
+    def initialize(word)
+        @anagram = word
+    end 
+
+    def match(array)
+        new_array = []
+        array.collect do |words|
+            if words.split("").sort == @anagram.split("").sort
+                new_array << words
+            end
+        end
+        new_array
+    end 
+    
+end 
